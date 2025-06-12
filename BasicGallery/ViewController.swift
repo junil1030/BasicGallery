ec754dd203bff8dc3a9f65502e86811994e3556c
junil1030 <dccrdseo@naver.com>
2025-06-12 17:33:42 +0900

    Init Project

---
+++ BasicGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicGallery
+//
+//  Created by 서준일 on 6/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
